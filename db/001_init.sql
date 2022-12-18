a1aba0ee43beec28861731c7258b613c1c3fc4ce
mccaffreycarson <mccaffreycarson@gmail.com>
2022-12-17 23:52:46 -0500

    New db definition

--- db/001_init.sql
+++ db/001_init.sql
@@ -9,14 +9,12 @@ BEGIN
     CREATE TABLE IF NOT EXISTS Spotify.User (
         id CHAR(256) NOT NULL,
         name CHAR(256) NOT NULL,
-        uri CHAR(256) NOT NULL,
         PRIMARY KEY(id)
     );
 
     CREATE TABLE IF NOT EXISTS Spotify.Playlist (
         id CHAR(256) NOT NULL,
         name CHAR(256) NOT NULL,
-        uri CHAR(256) NOT NULL,
         user_id CHAR(256) NOT NULL,
         PRIMARY KEY (id),
         FOREIGN KEY (user_id) REFERENCES Spotify.User(id)
@@ -24,18 +22,21 @@ BEGIN
 
     CREATE TABLE IF NOT EXISTS Spotify.Track (
         id CHAR(256) NOT NULL,
+        playlist_id CHAR(256) NOT NULL,
         name CHAR(256) NOT NULL,
         artist CHAR(256) NOT NULL,
-        PRIMARY KEY (id)
+        PRIMARY KEY (id, playlist_id),
+        FOREIGN KEY (playlist_id) REFERENCES Spotify.Playlist(id)
     );
 
     CREATE TABLE IF NOT EXISTS Spotify.Vote (
-        user_id CHAR(256) NOT NULL,
+        playlist_id CHAR(256) NOT NULL,
         track_id CHAR(256) NOT NULL,
+        user_id CHAR(256) NOT NULL,
         vote CHAR(1) NOT NULL,
-        PRIMARY KEY(user_id, track_id),
-        FOREIGN KEY (user_id) REFERENCES Spotify.User(id),
-        FOREIGN KEY (track_id) REFERENCES Spotify.Track(id)
+        PRIMARY KEY(playlist_Id, track_id, user_id),
+        FOREIGN KEY (playlist_id, track_id) REFERENCES Spotify.Track(playlist_id, id),
+        FOREIGN KEY (user_id) REFERENCES Spotify.User(id)
     );
 
 END;
