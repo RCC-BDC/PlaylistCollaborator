c42993c687aae8563456cfdf8d8ec9ecf2d46e2b
mccaffreycarson <mccaffreycarson@gmail.com>
2022-12-17 23:01:45 -0500

    First draft of DB

--- db/001_init.sql
+++ db/001_init.sql
@@ -6,8 +6,36 @@ BEGIN
 
     RAISE NOTICE 'Creating Tables';
 
-    CREATE TABLE IF NOT EXISTS Spotify.Playlist (
+    CREATE TABLE IF NOT EXISTS Spotify.User (
+        id CHAR(256) NOT NULL,
+        name CHAR(256) NOT NULL,
+        uri CHAR(256) NOT NULL,
+        PRIMARY KEY(id)
+    );
 
+    CREATE TABLE IF NOT EXISTS Spotify.Playlist (
+        id CHAR(256) NOT NULL,
+        name CHAR(256) NOT NULL,
+        uri CHAR(256) NOT NULL,
+        user_id CHAR(256) NOT NULL,
+        PRIMARY KEY (id),
+        FOREIGN KEY (user_id) REFERENCES Spotify.User(id)
+    );
+
+    CREATE TABLE IF NOT EXISTS Spotify.Track (
+        id CHAR(256) NOT NULL,
+        name CHAR(256) NOT NULL,
+        artist CHAR(256) NOT NULL,
+        PRIMARY KEY (id)
+    );
+
+    CREATE TABLE IF NOT EXISTS Spotify.Vote (
+        user_id CHAR(256) NOT NULL,
+        track_id CHAR(256) NOT NULL,
+        vote CHAR(1) NOT NULL,
+        PRIMARY KEY(user_id, track_id),
+        FOREIGN KEY (user_id) REFERENCES Spotify.User(id),
+        FOREIGN KEY (track_id) REFERENCES Spotify.Track(id)
     );
 
 END;
